adc5637960c5c682f5d20c91a205f24da6acf683
preyav <preya@serviceclusters.com>
2025-12-24 11:26:22 -0600

    Polish UI for listings and detail pages

--- listings/urls.py
+++ listings/urls.py
@@ -4,8 +4,6 @@ from .views import ListingListView, ListingDetailView
 app_name = "listings"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("pages.urls", namespace="pages")),
     path("listings/", include("listings.urls", namespace="listings")),
     path("listings/", ListingListView.as_view(), name="listing_list"),
     path("listings/<int:pk>/", ListingDetailView.as_view(), name="listing_detail"),
