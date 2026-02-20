cbb9650cbc69327014e6ab04bd9dcebdb00159bb
preyav <preya@serviceclusters.com>
2026-02-20 12:29:56 -0600

    Polish UI for listings and detail pages

--- config/context_processors.py
+++ config/context_processors.py
@@ -1,6 +1,7 @@
 import os
+from django.conf import settings
 
 def export_vars(request):
     return {
-        'GOOGLE_MAPS_API_KEY': os.environ.get('GOOGLE_MAPS_API_KEY')
+        "GOOGLE_MAPS_API_KEY": getattr(settings, "GOOGLE_MAPS_API_KEY", ""),
     }
