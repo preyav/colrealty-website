bb96993551a1898baf829c4f7aa3ef1b39126208
preyav <preya@serviceclusters.com>
2026-02-22 11:38:37 -0600

    Polish UI for listings and detail pages

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 # config/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,9 +12,4 @@ urlpatterns = [
     path("leads/", include("leads.urls")),
 
 ]
-
-
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
