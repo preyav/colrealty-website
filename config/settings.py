644a8c17f58cd41289849c0a9a478ca29d62b9f3
preyav <preya@serviceclusters.com>
2026-06-25 00:20:35 +0000

    Stop tracking MLS listing media and fix S3 storage config

--- config/settings.py
+++ config/settings.py
@@ -282,8 +282,8 @@ else:
     STATIC_ROOT = BASE_DIR / "staticfiles"
     STATICFILES_DIRS = [BASE_DIR / "static"]
 
-    MEDIA_URL = "/media/"
-    MEDIA_ROOT = BASE_DIR / "media"
+#    MEDIA_URL = "/media/"
+#    MEDIA_ROOT = BASE_DIR / "media"
 
     STORAGES = {
         "default": {
